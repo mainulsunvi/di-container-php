b1ff496bd827ec7b67f1ae969a430f20d077b31b
mainulsunvi <mainulsunvi@gmail.com>
2023-08-23 11:30:12 +0300

    Convert Autoload from Functional to OOP Style

--- loader/autoload.php
+++ loader/autoload.php
@@ -1,23 +1,36 @@
 <?php
 
-function classAutoloader( $class ) {
-    $prefix   = 'Inc\\';
-    $base_dir = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+class Autoload {
+    private string $prefix;
+    private string $base_dir;
+    private string $full_path;
 
-    $class = ( str_contains( $class, '_' ) ) ? str_replace( '_', '-', $class ) : $class;
+    function __construct() {
+        spl_autoload_register( array( $this, "classAutoloader" ) );
 
-    $class = substr( $class, strlen( $prefix ) );
-    $class = str_replace( '\\', DIRECTORY_SEPARATOR, $class );
-
-    if ( preg_match( '/([^\\\\]+)$/', $class, $matches ) ) {
-        $dirname = rtrim( $class, $matches[0] );
+        $this->base_dir = dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+        $this->prefix   = 'Inc\\';
     }
 
-    $full_path = $base_dir . strtolower( $dirname ) . $matches[0] . '.php';
+    function classAutoloader( $className ) {
 
-    if ( file_exists( $full_path ) ) {
-        require_once $full_path;
+        $className = ( str_contains( $className, '_' ) ) ? str_replace( '_', '-', $className ) : $className;
+
+        $className = substr( $className, strlen( $this->prefix ) );
+        $className = str_replace( '\\', DIRECTORY_SEPARATOR, $className );
+
+        if ( preg_match( '/([^\\\\]+)$/', $className, $matches ) ) {
+            $dirname = rtrim( $className, $matches[0] );
+        }
+
+        $this->full_path = $this->base_dir . strtolower( $dirname ) . $matches[0] . '.php';
+
+        if ( file_exists( $this->full_path ) ) {
+            require_once $this->full_path;
+        }
     }
 }
 
-spl_autoload_register( "classAutoloader" );
+if ( class_exists( 'Autoload' ) ) {
+    new Autoload();
+}
